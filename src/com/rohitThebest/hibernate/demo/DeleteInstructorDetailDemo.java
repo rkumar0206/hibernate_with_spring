df09f7843703889056f052e913efbf4e700772b1
Rohit <rkumar8092378845@gmail.com>
2021-10-06 13:03:07 +0530

    deleting the instructor detail object which will also delete the instructor associated with it

---
+++ src/com/rohitThebest/hibernate/demo/DeleteInstructorDetailDemo.java
@@ -0,0 +1,66 @@
+package com.rohitThebest.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import com.rohitThebest.hibernate.demo.entity.Instructor;
+import com.rohitThebest.hibernate.demo.entity.InstructorDetail;
+
+public class DeleteInstructorDetailDemo {
+
+	public static void main(String[] args) {
+		
+		// create session factory
+		SessionFactory sessionFactory = new Configuration()
+											.configure("hibernate.cfg.xml")
+											.addAnnotatedClass(Instructor.class)
+											.addAnnotatedClass(InstructorDetail.class)
+											.buildSessionFactory();
+
+		// create session
+		Session session = sessionFactory.getCurrentSession();
+		
+		try {
+
+		
+			// start a transaction
+			session.beginTransaction();
+		
+			
+			// get the instructor detail object
+			int id = 2;
+			InstructorDetail instructorDetail = session.get(InstructorDetail.class, id);
+			
+			// print the instructor detail
+			System.out.println("instructor detail: " + instructorDetail);
+			
+			// print the associated instructor
+			System.out.println("the associated instructor : " + instructorDetail.getInstructor());
+		
+			
+			// now let's delete the instructor detail
+			// Note : It will also delete the Instructor associated with the 
+			// instructor detail because of Cascade.ALL
+			//
+			System.out.println("Deleting instructorDetail : " + instructorDetail);
+			session.delete(instructorDetail);
+			
+			
+			// commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done");
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			
+			// handle connection leak issue
+			session.close();			
+			sessionFactory.close();
+		}
+		
+	}
+
+}
+
